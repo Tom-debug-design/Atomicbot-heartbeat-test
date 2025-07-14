c2b806dcf7f5fa979e0b209c794f8fd4f7c76814
Tom-debug-design <oletomhanssen65@gmail.com>
2025-07-14 23:25:15 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 
 app = Flask(__name__)
 
-DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK", "https://discord.com/api/webhooks/1391855933071560735/uH6LYuqM6uHLet9KhsgCS89fQikhyuPRJmjhqmtESMHaLu3LxDfUrVggwxzSGyscEtiN")
+DISCORD_WEBHOOK = os.getenv("DISCORD_WEBHOOK", "https://discord.com/api/webhooks/1391855933071560735/uH6LYuqM6uHLet9KhsgCS89fQikhyuPRJmjhqmtESMhAlu3LxDfUrVggwxzSGyscEtiN")
 
 @app.route("/")
 def home():
@@ -25,4 +25,4 @@ def webhook():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=False, host="0.0.0.0", port=port)
